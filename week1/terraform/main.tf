14709d5cbc88bde87c586f4b78d75053196134c4
Полетаев Валентин Вячеславович <vpolet@mts.ru>
2023-01-30 21:08:50 +0300

    Homework week1 - terraform

---
+++ week1/terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+    }
+  }
+}
+
+provider "local" {
+  project = var.project
+  region = var.region
+  // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
+}
